580154986b588bdb5fad58b08a431c3198fb9c2a
Md Al-Amin <md.al.amin@portonics.com>
2025-12-02 21:17:30 +0600

    Add Test Case No 5

---
+++ pages/create_page_using_shortcode.py
@@ -0,0 +1,55 @@
+import time
+from playwright.sync_api import Page
+
+class CreateNewPage:
+    def __init__(self, page: Page):
+        self.page = page
+
+        # Create Page
+        self.flextable_button_from_menu =  page.locator("//div[normalize-space()='FlexTable']")
+        self.short_code_button = page.locator("//button[@class='copy-shortcode btn-shortcode ']")
+        self.page_from_menu = page.locator("//div[normalize-space()='Pages']")
+        self.add_new_page =  page.locator("//a[@href='post-new.php?post_type=page']")
+        self.choosea_pattern = page.locator("//div[@class='components-modal__header']//button[@aria-label='Close']//*[name()='svg']")
+        self.select_block = page.locator("//span[normalize-space()='Block']")
+        self.page_title = page.locator("//h1[@aria-label='Add title']")
+        self.add_block_button = page.locator("//button[@aria-label='Add block']")
+        self.search_filed = page.locator("input.components-input-control__input[placeholder='Search']")
+        self.select_shortcode = page.locator("//span[contains(text(),'Shortcode')]")
+        self.enter_shortcode_field = page.get_by_placeholder("Write shortcode here…")
+        self.publish_button = page.locator("//button[normalize-space()='Publish']")
+        self.second_publish_button = page.locator("//button[@class='components-button editor-post-publish-button editor-post-publish-button__button is-primary is-compact']")
+        self.view_page_button = page.locator("//a[@class='components-button is-next-40px-default-size is-primary']")
+
+        # verify table
+        self.can_not_load_text = page.locator("//b[contains(text(),'Table maybe deleted or can’t be loaded.')]")
+
+    def create_new_page(self):
+        self.page.reload()
+        time.sleep(3)
+        short_code = self.short_code_button.all_text_contents()[0]
+        print("Code is", short_code)
+        self.page_from_menu.click()
+        self.add_new_page.click()
+        self.choosea_pattern.click()
+        self.page_title.fill("Test Page")
+        self.add_block_button.click()
+        time.sleep(1)
+        self.search_filed.fill("Shortcode")
+        time.sleep(1)
+        self.select_shortcode.click()
+        self.enter_shortcode_field.fill(short_code)
+        time.sleep(1)
+        self.publish_button.click()
+        time.sleep(3)
+        self.second_publish_button.click()
+        self.view_page_button.click()
+        time.sleep(5)
+        self.page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(10)
+    
+    def verify_table_value(self):
+        if self.can_not_load_text.is_visible():
+            print("Table Can not loaded")
+        else:
+            print("Table loaded")
--- tests/test_FlexTable_Plugin.py
+++ tests/test_FlexTable_Plugin.py
@@ -5,6 +5,7 @@ from pages.create_new_table import CreateNewTable
 from pages.verify_login_functionality import VerifyLoginFunctionality
 from pages.verify_plugin_status import VerifyPluginStatusPage
 from pages.navigate_to_flexiable_dashboard import NavigateToFlexiableDashboard
+from pages.create_page_using_shortcode import CreateNewPage
 
 # Load credentials from .env
 load_dotenv()
@@ -18,6 +19,7 @@ def test_wordpress_login(page):
     plugin_page = VerifyPluginStatusPage(page)
     flextable_page = NavigateToFlexiableDashboard(page)
     create_table_page = CreateNewTable(page)
+    create_page =  CreateNewPage(page)
 
     # Test Case 1
     login_page.navigate(base_url)
@@ -35,5 +37,9 @@ def test_wordpress_login(page):
     # Test Case 4
     create_table_page.Create_Table_Using_Google_sheet("WPPOOL","Table Description")
     create_table_page.Verify_Table_Title("WPPOOL")
+
+    # Test Case 5
+    create_page.create_new_page()
+    create_page.verify_table_value()
 
  
